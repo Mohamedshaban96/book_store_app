c110efa11d52ada05d183c046d2fccb91876b9b5
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2025-01-27 04:14:59 +0200

    adding necessary files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./Components/style.css";
+import React from "react";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
